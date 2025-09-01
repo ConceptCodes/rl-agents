e38eef1c6c13d3187a80431f72ebe14c42c43de7
concept <conceptcodes@gmail.com>
2025-09-01 09:01:08 -0500

    Refactor piece size and enhance player initialization; update rendering logic and improve grid handling

--- src/gungi/game.py
+++ src/gungi/game.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 import numpy as np
 from constants import (
@@ -13,8 +14,12 @@ from constants import (
     WHITE,
 )
 
+
 pygame.init()
-font = pygame.font.Font("freesansbold.ttf", 32)
+pygame.font.init()
+
+
+font = pygame.font.SysFont("AppleGothic", 22)
 
 
 class Piece:
@@ -37,9 +42,25 @@ class Piece:
         return True
 
     def render(self, screen):
+        if self.x is None or self.y is None:
+            return
+
         pygame.draw.circle(screen, self.color, (self.x, self.y), PIECE_SIZE)
-        text = font.render(self.symbol, True, BLACK)
-        screen.blit(text, (self.x, self.y))
+
+        text_surf = font.render(
+            self.symbol, True, BLACK if self.color == WHITE else WHITE
+        )
+
+        text_rect = text_surf.get_rect(center=(self.x, self.y))
+        screen.blit(text_surf, text_rect)
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            return [self] * other
+        return NotImplemented
+
+    def __repr__(self):
+        return f"<Piece(symbol={self.symbol}, color={self.color}, position=({self.x}, {self.y}))>"
 
 
 class Player:
@@ -47,20 +68,31 @@ class Player:
         self.color = color
         self.pieces = self._init_pieces()
 
-    def _init_pieces(self):
+    def _init_pieces(self) -> list[Piece]:
         return [
             Piece("MARSHAL", self.color),
             Piece("GENERAL", self.color),
             Piece("LIEUTENANT_GENERAL", self.color),
-            Piece("MAJOR_GENERAL", self.color) * 2,
-            Piece("WARRIOR", self.color) * 2,
-            Piece("LANCER", self.color) * 3,
-            Piece("RIDER", self.color) * 2,
-            Piece("SPY", self.color) * 2,
-            Piece("FORTRESS", self.color) * 2,
-            Piece("SOLDIER", self.color) * 4,
+            Piece("MAJOR_GENERAL", self.color),
+            Piece("MAJOR_GENERAL", self.color),
+            Piece("WARRIOR", self.color),
+            Piece("WARRIOR", self.color),
+            Piece("LANCER", self.color),
+            Piece("LANCER", self.color),
+            Piece("LANCER", self.color),
+            Piece("RIDER", self.color),
+            Piece("RIDER", self.color),
+            Piece("SPY", self.color),
+            Piece("SPY", self.color),
+            Piece("FORTRESS", self.color),
+            Piece("FORTRESS", self.color),
+            Piece("SOLDIER", self.color),
+            Piece("SOLDIER", self.color),
+            Piece("SOLDIER", self.color),
+            Piece("SOLDIER", self.color),
             Piece("CANNON", self.color),
-            Piece("ARCHER", self.color) * 2,
+            Piece("ARCHER", self.color),
+            Piece("ARCHER", self.color),
             Piece("MUSKETEER", self.color),
             Piece("TACTICIAN", self.color),
         ]
@@ -71,7 +103,7 @@ class Game:
         self.is_running = True
         self.clock = pygame.time.Clock()
         self.render_ui = render_ui
-        self.grid = np.zeros((BOARD_SIZE, BOARD_SIZE), dtype=int)
+        self.grid = np.zeros((BOARD_SIZE, BOARD_SIZE), dtype=tuple)
 
         self.player_1 = Player(BLACK)
         self.player_2 = Player(WHITE)
@@ -86,6 +118,16 @@ class Game:
     def end(self):
         pygame.quit()
         exit()
+
+    def _init_player_positions(self, player: Player):
+        # this should use a random layout
+        # current setup doesn't take piece stacking in effect
+        for piece in player.pieces:
+            for col in range(BOARD_SIZE):
+                for row in range(3):
+                    x, y = self.grid[col][row + (0 if player.color == WHITE else 6)]
+                    piece.set_position(x, y)
+                    piece.render(self.screen)
 
     def start(self):
         while self.is_running:
@@ -104,7 +146,10 @@ class Game:
             for y in range(0, HEIGHT, blockSize):
                 rect = pygame.Rect(x, y, blockSize, blockSize)
                 pygame.draw.rect(self.screen, BORDER_COLOR, rect, 1)
-                self.grid[cols][rows] = (x + blockSize // 2, y + blockSize // 2)
+                self.grid[cols][rows] = (
+                    int(x + blockSize // 2),
+                    int(y + blockSize // 2),
+                )
                 rows += 1
                 if rows == BOARD_SIZE:
                     rows = 0
@@ -114,6 +159,9 @@ class Game:
         if self.render_ui:
             self.screen.fill(BOARD_COLOR)
             self._draw_borders()
+
+            self._init_player_positions(self.player_1)
+            self._init_player_positions(self.player_2)
 
             pygame.display.flip()
 
